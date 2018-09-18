1a70d0e40a75cadf7ee6ffee75d1e5836012a84e
mei-li <meili.tr@gmail.com>
2018-09-19 00:23:05 +0200

    improve task explanations

--- workshops/20_python_beginners_workshop/tasks/task_5_dictionaries.py
+++ workshops/20_python_beginners_workshop/tasks/task_5_dictionaries.py
@@ -6,8 +6,8 @@ Dictionaries!
 Dictionaries are super useful python data structures and if you are dealing with data, like
 wikipedia data, questionaire data, or anything you can imagine, dictionaries will prove useful.
 
-A. Let's use a dictionary to describe each room. Set a variable called door_greetings and is a dictionary
-with keys the door number and values the door greeting. eg. door_greetings = {'1': "Welcome to the paradise"}
+A. Let's use a dictionary to describe each room. Create a dictionary variable called door_greetings
+with keys the door numbers and values the door greeting. eg. door_greetings = {'1': "Welcome to the paradise"}
 B. When the user enters each room print the corresponding door greeting from the dictionary.
 '''
 
--- workshops/20_python_beginners_workshop/tasks/task_7_optional_dict_advanced.py
+++ workshops/20_python_beginners_workshop/tasks/task_7_optional_dict_advanced.py
@@ -1,31 +1,41 @@
 """
 Task 7
 
-Use the dictionary adventure below to control the game play.
-In the code below there a complex dictionary named adventure that has as values dictionaries as well.
+Use the dictionary adventure below to control the game play instead of if-else statements.
+
+This task combines for-loops, complex dictionaries and lists. It is recommended after the
+concepts of loops and dictionaries and lists are pretty well understood.
+
+In the code below there a complex dictionary named `adventure` that has as values dictionaries as well.
 This complex dictionary includes all the text needed to play the game. The value of a door eg door '1' is
 also a dictionary, with keys "greeting" that is the text to show when the user enters the room and
 "options" which is a list of dictionaries with the "action" to display and then the "result" to show to the
 user when they choose this option. Currently only the door 1 is defined.
 
 
-A. Take some time to understand the structure of this dictionary. Continue the program in the
-indicated line and print the greeting to the chosen door. Print the greeting of the choosen door,
-using the value from the dictionary. eg. the greeting of the door '1' can be accessed with adventure['1']['greeting']
-or if the door is a variable called 
+A. Take some time to understand the structure of the dictionary adventure in the code below. Copy this code to a new file
+and continue the program in the indicated line and print the greeting of the chosen door,
+using the value from the dictionary
+ eg. the greeting of the door '1' can be accessed with adventure['1']['greeting'] or if the
+ door number is in a variable called door, adventure[door]['greeting'] will get the greeting for the
+ variable door from the dictionary. This value can be passed directly into a print statement.
 
-Tip: Since now adventure['1'] is a dictionary you can use adventure['1']['greeting'] or
-adventure['1']['options'] to get the values of that part. Even better so that it works for any
-chosen door the variable `door` can be used. eg. adventure[door]['options']
-
-B. ExaclyPrint after the greeting the possible actions for each option of the chosen door. Eg.
+B. Exactly after the print of the greeting print the possible actions for each option of the chosen door. Eg.
 Options:
  1. Smile and node
  2. Scream and run
-Tip: For loop is needed to go through the list of options. To show the number of each option python enumerate
-function can be useful, http://book.pythontips.com/en/latest/enumerate.html
+Tips:
+    * Accessing the action of the first option of the first door can be done with adventure['1']['options'][0]['action']
+    * For loop is needed to go through the list of options.
+    * To show the number of each option python enumerate function can be useful,
+    http://book.pythontips.com/en/latest/enumerate.html
 
-C. If the chosen door is not available in adventure show a message.
+
+C. Add more options to door '1'.
+D. Add more doors to adventure dictionary.
+Tip: Copy paste the structure of door '1' and change the values.
+
+E. If the chosen door is not available in adventure show a message.
 Tip to check if a value is one of the dictionary keys, the "in" or the "not in" can be used.
 eg. if door in adventure
 """
@@ -42,9 +52,10 @@ adventure = {
                 "action": "Scream and run",
                 "result": "Sorry the vampire is faster, you are dead!"
             },
+            # Exercise C
         ]
     },
-
+    # Exercise D
 }
 
 
